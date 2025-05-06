e3e4513df97af412e95ecefc2ccd8ed41e983beb
Lucas <lucas.villaboa@gmail.com>
2025-05-06 13:51:14 -0300

    Fix:carga de assets con HTTPS para producción

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-      buildDirectory: 'build',
-    }),
-  ],
-  build: {
-    outDir: 'public/build',
-    assetsDir: 'assets', // o simplemente quitá esta línea
-  },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    build: {
+        outDir: 'public/build',
+    },
+    base: '/build/', // importante
 });
