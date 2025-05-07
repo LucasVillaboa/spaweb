1b8f1e64240fed4d6ede6424453ca706fa67f109
Lucas <lucas.villaboa@gmail.com>
2025-05-07 15:04:56 -0300

    corrijo vite config y vista

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,26 +6,22 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+
+
     <title>Spa Natural</title>
 
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Bootstrap Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 <body>
-    {{-- Navbar (si tenés un partial separado, descomentá esta línea) --}}
-    {{-- @include('partials.navbar') --}}
+    @include('partials.navbar')
 
     <div class="container mt-4">
         @yield('content')
     </div>
 
-    {{-- Footer (si tenés un partial separado, descomentá esta línea) --}}
-    {{-- @include('partials.footer') --}}
+    @include('partials.footer')
 
-    <!-- Bootstrap JS (opcional) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
