594d1716dea07d34211217d1be9ddca21c4b7b05
Lucas <lucas.villaboa@gmail.com>
2025-05-08 14:57:46 -0300

    cambio posicion de @vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
-
-
-
+   
     <title>Spa Natural</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     @include('partials.navbar')
@@ -23,6 +22,6 @@
     @include('partials.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
 </body>
 </html>
