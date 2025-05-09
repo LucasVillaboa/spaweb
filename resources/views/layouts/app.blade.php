05ce48a41b9244a545533d9e191869f12cbdd2db
Lucas <lucas.villaboa@gmail.com>
2025-05-09 16:22:35 -0300

    actializacion app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
    
     <title>Spa Natural</title>
-
-    
-    @vite(['resources/css/app.css',  'resources/js/app.js'])
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    
+    @vite(['resources/css/app.css',  'resources/js/app.js'])
+
 </head>
 <body>
     @include('partials.navbar')
