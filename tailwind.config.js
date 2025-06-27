6bfa3b3f09b7a4b27753d99715df60d1826233dc
Lucas Villaboa <lucas.villaboa@gmail.com>
2025-06-27 01:38:14 -0300

    actualizo tailwaind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
-import forms from '@tailwindcss/forms'
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
+/** @type {import('tailwindcss').Config} */
 export default {
+
+content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/*/.blade.php',
+    './resources/js/*/.js', // AGREGADO: analiza los JS por clases Tailwind
+],
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.js',
     ],
+
 
     theme: {
         extend: {
@@ -18,4 +27,4 @@ export default {
     },
 
     plugins: [forms],
-}
+};
