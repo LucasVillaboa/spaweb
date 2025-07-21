3168e6d34add3a96fe8f2c73f08e65f300cfba5d
Lucas Villaboa <lucas.villaboa@gmail.com>
2025-07-21 20:39:25 -0300

    se edita nombre spa footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-dark text-white py-4 mt-5">
     <div class="container text-center">
-        <p class="mb-1">© 2025 Spa Natural. Todos los derechos reservados.</p>
+        <p class="mb-1">© 2025 Spa Sentirse Bien. Todos los derechos reservados.</p>
         <p class="mb-2">Dirección: Av. Sarmiento 123, Resistencia, Chaco. Hipólito Yrigoyen 325, Corrientes</p>
         <p class="mb-2">Horarios: Lunes a Viernes de 9:00 a 20:00 hs</p>
         
